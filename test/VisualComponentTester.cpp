3df70ab1cb0ce68cafb1eae3bb307a7105653d5d
Jathn <jonathan.ramstedt@gmail.com>
2024-04-17 18:48:28 +0300

    Added testing for entire screen

--- test/VisualComponentTester.cpp
+++ test/VisualComponentTester.cpp
@@ -3,7 +3,9 @@
 VisualComponentTester::VisualComponentTester() : Tester() {}
 
 std::pair<int, int> VisualComponentTester::runTest() {
-    std::shared_ptr<Tile> tile = std::make_shared<Tile>(0, 0, 0, std::vector<std::shared_ptr<Tile>>(), false, TileType::FOREST);
+    GameStateManager gameStateManager(std::make_pair<int, int>(13, 9));
+    GameGraphicsManager gameGraphicsManager(gameStateManager);
+    std::shared_ptr<Tile> tile = gameStateManager.getMap()[0];
     std::shared_ptr<Territory> territory = std::make_shared<Territory>();
     MovementHandler movementHandler = MovementHandler(territory);
     for (int i = 0; i < 9; i++) {
@@ -12,9 +14,7 @@ std::pair<int, int> VisualComponentTester::runTest() {
         unit->setTile(tile);
         movementHandler.addUnit(unit);
     }
-    std::shared_ptr<Tile> tile2 = std::make_shared<Tile>(1, 1, 0, std::vector<std::shared_ptr<Tile>>({tile}), false, TileType::GRASS);
-    TileView tileView(tile);
-    TileView tileView2(tile2);
+    std::shared_ptr<Tile> tile2 = gameStateManager.getMap()[1];
     tile->addNeighbor(tile2);
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "TileView Test");
     while (window.isOpen()) {
@@ -28,28 +28,8 @@ std::pair<int, int> VisualComponentTester::runTest() {
             }
         }
         window.clear();
-        tileView.draw(window);
-        tileView2.draw(window);
+        gameGraphicsManager.draw(window);
         window.display();
-    }
-
-    std::shared_ptr<Player> player = std::make_shared<Player>();
-
-    PlayerView playerView(player, std::pair<int, int>(0, 0), std::pair<int, int>(400, 200));
-    sf::RenderWindow window2(sf::VideoMode(1920, 1080), "PlayerView Test");
-    while (window2.isOpen()) {
-        sf::Event event;
-        while (window2.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window2.close();
-            }
-            if (event.type == sf::Event::MouseButtonReleased) {
-                player->addResource(ResourceType::GOLD, 100);
-            }
-        }
-        window2.clear();
-        playerView.draw(window2);
-        window2.display();
     }
 
     return std::pair<int, int>(0, 0);
