2c573b4db92c5472fcf2b9a34889e7fdf490005a
Jathn <jonathan.ramstedt@gmail.com>
2024-06-10 20:31:56 +0300

    removed excess debugging info

--- src/phases/BuyPhase.cpp
+++ src/phases/BuyPhase.cpp
@@ -3,10 +3,8 @@
 
 BuyPhase::BuyPhase(const std::shared_ptr<GameStateManager>& state_manager, const std::shared_ptr<GameGraphicsManager>& graphics_manager) :
     Phase(state_manager, graphics_manager, "Buy Phase") {
-        std::cout << "Buy Phase Init" << std::endl;
         buy_window_ = BuyWindow();
         confirm_button_ = std::make_shared<Button>("Confirm Purchases", std::pair<int, int>(250, 50), std::pair<int,int>(650, 850));
-        std::cout << "Buy Phase Init Done" << std::endl;
     }
 
 void BuyPhase::handleEvent(sf::Event& event, sf::RenderWindow& window) {
@@ -24,11 +22,8 @@ void BuyPhase::handleEvent(sf::Event& event, sf::RenderWindow& window) {
 
 void BuyPhase::draw(sf::RenderWindow& window) {
     // Draw the buy window
-    std::cout << "Drawing buy_window_" << std::endl;
     buy_window_.draw(window);
-    std::cout << "Drawing confirm_button_" << std::endl;
     confirm_button_->draw(window);
-    std::cout << "Drawing done" << std::endl;
 }
 
 std::shared_ptr<Phase> BuyPhase::getNextPhase() {
